11ca158884448447f5d6e80fd2eb2d852093f35b
J.M. Vishal Rashmika Perera <87840726+VishalRashmika@users.noreply.github.com>
2024-06-27 19:01:39 +0530

    added the csv_reading functionality

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from pygoogle_image import image as pi
 
-pi.download("gtr r34",limit=5)
-pi.download("gtr r34",limit=5,directory='./images')
+
+# pi.download("gtr r34",limit=5,directory='../images')
+
+with open("test.csv") as file_in:
+    lines = []
+    for line in file_in:
+        pi.download(line,limit=2)
+        
