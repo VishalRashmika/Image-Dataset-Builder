0a91798cbbde33c1fd37af2d08a3c3c0d4209f3a
J.M. Vishal Rashmika Perera <87840726+VishalRashmika@users.noreply.github.com>
2024-06-27 19:34:06 +0530

    code clean

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-number_of_images = 2
+from pygoogle_image import image as pi
 
-try:
-    from pygoogle_image import image as pi
 
-    with open("test.csv") as file_in:
-        lines = []
-        for line in file_in:
-            pi.download(line,limit=number_of_images)
-            print(f"-- {line} :: download completed ---")
+def main():
+    number_of_images = int(input("number of images per entry: "))
+    try:
 
-    print("Process Successfully Completed")
-except:
-    print("Error occured")
+        with open("test.csv") as file_in:
+            lines = []
+            for line in file_in:
+                pi.download(line,limit=number_of_images)
+                print(f"-- {line} :: download completed ---")
+
+        print("Process Successfully Completed")
+    except:
+        print("An Error occured")
+
+main()
