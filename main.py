632ccab1bf656e878caed4278f567a321a70cc20
J.M. Vishal Rashmika Perera <87840726+VishalRashmika@users.noreply.github.com>
2024-06-27 19:07:36 +0530

    status output

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from pygoogle_image import image as pi
 
-
-# pi.download("gtr r34",limit=5,directory='../images')
-
 with open("test.csv") as file_in:
     lines = []
     for line in file_in:
-        pi.download(line,limit=2)
-        
+        pi.download(line,limit=1)
+        print(f"-- {line} :: download completed ---")
