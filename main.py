dc647fa4359129c53f26ab4184fc522c5eb6fd58
J.M. Vishal Rashmika Perera <87840726+VishalRashmika@users.noreply.github.com>
2024-06-27 19:17:56 +0530

    updates

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from pygoogle_image import image as pi
 with open("test.csv") as file_in:
     lines = []
     for line in file_in:
-        pi.download(line,limit=1)
+        pi.download(line,limit=3)
         print(f"-- {line} :: download completed ---")
+
+print("Process Successfully Completed")
