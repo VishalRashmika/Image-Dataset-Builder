531066689d4fe872e58b17c6d17d0cafcc14370d
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-06-28 18:29:07 +0530

    partially working gui

--- ui.py
+++ ui.py
@@ -2,6 +2,7 @@ from PyQt6.QtWidgets import *
 from PyQt6 import uic
 
 class MyGUI(QMainWindow):
+    fileName = ""
     def __init__(self):
         super(MyGUI,self).__init__()
         uic.loadUi("form.ui",self)
@@ -13,16 +14,25 @@ class MyGUI(QMainWindow):
 
 
     def get_file_location(self):
-        fileName = QFileDialog.getOpenFileName(self,"Open File","./","All Files (*);;Python Files (*.py);; PNG Files (*.png)",)
-        self.txtLocation.insertPlainText(fileName[0])
+        self.fileName = QFileDialog.getOpenFileName(self,"Open File","./","All Files (*);;Python Files (*.py);; PNG Files (*.png)",)[0]
+        self.txtLocation.insertPlainText(self.fileName)
+
 
     def load_content(self):
         self.lstEntries.setEnabled(True)
-        file = open('test.txt', 'r')
+        self.lstLog.setEnabled(True)
+        self.lstLog.addItem(f"file loaded from : {str(self.fileName)}")
 
+        file = open(str(self.fileName), 'r')
         for entry in file:
-            print(entry)
-            self.lstEntries.addItem(QListWidgetItem(entry))
+            entry = entry.strip()
+            self.lstEntries.addItem(entry)
+        
+        number_of_entries = self.lstEntries.count()
+        number_of_images = self.txtEntries.toPlainText()
+        self.lstLog.addItem(f"No. of entries loaded : {number_of_entries}")
+        self.lstLog.addItem(f"Number of images to be downloaded : {number_of_entries} x {number_of_images} = {int(number_of_entries) * int(number_of_images)}")
+
 
 
 def main():
